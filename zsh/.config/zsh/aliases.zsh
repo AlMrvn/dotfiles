68da95024889ea3bf3d6ec4b9a120b5d1f00d93b
Alexis Morvan <morvan.alexis@gmail.com>
2024-07-30 13:35:39 -0700

    adding some aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -5,6 +5,8 @@ alias g=git
 alias :q=exit
 alias ..='cd ..'
 alias ....='cd ../..'
+alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc' # Quick access to the .zshrc file
+alias dotfiles="cd ~/.dotfiles"
 
 # fun stuff
 # # Search through your command history and print the top 10 commands
